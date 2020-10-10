d869b289fd445240ce3b2ed449888793f7c244ff
matttsb <matttsburke@gmail.com>
2020-10-11 00:13:41 +0200

    real

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/matt/work/websites/plumrzlive/usr/local/lsws/plumrz.com/html/django/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
