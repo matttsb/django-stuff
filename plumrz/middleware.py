216f0158bb80be1c2575352549106fc9b2b36cee
matttsb <matttsburke@gmail.com>
2020-12-08 03:54:55 +0100

    fix-missing-image-tags3-commit

--- plumrz/middleware.py
+++ plumrz/middleware.py
@@ -11,27 +11,33 @@ class FixStuffMiddleware:
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-
         response = self.get_response(request)
-        if response['Content-Type'] == "text/html; charset=utf-8":
+        try:
+            contenttype=response['Content-Type']
+        except:
+            contenttype=""
+        
+        if contenttype == "text/html; charset=utf-8":
             response_text=str(response.content,encoding='UTF-8')
 #            response_text=response_text.replace('and','xxx')
-            soup = BSHTML(response_text)
+            soup = BSHTML(response_text,features="lxml")
             images = soup.findAll('img')
-            im=""
+            title=""
+            alt=""
             for image in images:
   
                 try:
-                    if image['alt']=="":
-                        image['alt']=image['src'].replace("/media/img/","").replace(".jpg","").replace("_"," ").replace("-"," ") 
+                    alt = image['alt']
                 except:
-                        print("oops")
+                    if not alt:
+                        alt=image['src'].replace("/media/img/","").replace(".jpg","").replace("_"," ").replace("-"," ") 
                 
                 try:
-                    _title = (image['title'])
-                    im=image['alt']
+                    title = (image['title'])
                 except:
-                    image['title']= im
+                    if not title:
+                        image['title']=alt
+
             response_text=str(soup)
             response.content = bytes(response_text,encoding='UTF-8')
       
